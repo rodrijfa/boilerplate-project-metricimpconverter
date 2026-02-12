1d15b37a8c5fb34231c3d5b3db1e81d401ca0ac1
rodrijfa <rodrijfa@gmail.com>
2026-02-12 16:53:40 -0400

    final server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 
+require('dotenv').config();
+
 const express     = require('express');
 const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
+//const expect      = require('chai').expect;
 const cors        = require('cors');
-require('dotenv').config();
+//require('dotenv').config();
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -30,7 +32,7 @@ fccTestingRoutes(app);
 
 //Routing for API 
 apiRoutes(app);  
-    
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
@@ -41,19 +43,21 @@ app.use(function(req, res, next) {
 const port = process.env.PORT || 3000;
 
 //Start our server and tests!
-app.listen(port, function () {
-  console.log("Listening on port " + port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-          console.log('Tests are not valid:');
-          console.error(e);
-      }
-    }, 1500);
-  }
+if(!process.env.NODE_ENV || process.env.NODE_ENV === 'test') {
+  app.listen(port, function () {
+    console.log("Listening on port " + port);
+    if(process.env.NODE_ENV==='test') {
+      console.log('Running Tests...');
+      setTimeout(function () {
+        try {
+          runner.run();
+        } catch(e) {
+            console.log('Tests are not valid:');
+            console.error(e);
+        }
+      }, 1500);
+    }
 });
+}
 
 module.exports = app; //for testing
