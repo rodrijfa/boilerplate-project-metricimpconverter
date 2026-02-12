671f83d6cf43c2d940835ca668383235301d48c4
rodrijfa <rodrijfa@gmail.com>
2026-02-12 09:15:41 -0400

    Add API route for unit conversion

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,37 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert')
+    .get(function (req, res){
+
+      let input = req.query.input;
+
+      let initNum = convertHandler.getNum(input);
+      let initUnit = convertHandler.getUnit(input);
+
+      if(initNum === undefined && initUnit === undefined) {
+        return res.json('invalid number and unit');
+      }
+
+      if(initNum === undefined){
+        return res.json('invalid number');
+      }
+
+      if(initUnit === undefined){
+        return res.json('invalid unit');
+      }
+
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+      let returnNum = convertHandler.convert(initNum, initUnit);
+      let string = convertHandler.getString(initNum,initUnit,returnNum, returnUnit);
+
+      res.json({
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit,
+        string
+      });
+    });
+
 };
